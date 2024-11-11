253ae0e370919bbb55f6be4a0431c63d8aae89fd
blasaraujo14 <blas.araujo@udc.es>
2024-11-11 18:11:17 +0100

    done

--- encode.py
+++ encode.py
@@ -1,14 +1,19 @@
 import sys
 
-f1 = open(sys.argv[1])
-f2 = open(sys.argv[2], "w")
+inputFile = open(sys.argv[1]).readlines()
+outputFile = open(sys.argv[2], "w")
 
-x = f1.readlines()
-n = len(x[0]) - 1
-
-print (x)
+n = len(inputFile[0]) - 1
+outputFile.write("dim("+str(n)+").\n")
 
 for i in range(n):
-    for j in range(n):
-        if x[i][j]== 't':
-            print ("-> " + str(i) + str(j))
+	for j in range(n):
+		if inputFile[i][j]== 't':
+			outputFile.write("tree(" + str(i) + "," + str(j) + "). ")
+outputFile.write("\n")
+for i in range(n):
+	outputFile.write("rowVal(" + str(i) + "," + inputFile[-2][i*2] + "). ")
+outputFile.write("\n")
+for i in range(n):
+	outputFile.write("colVal(" + str(i) + "," + inputFile[-1][i*2] + "). ")
+outputFile.close()
